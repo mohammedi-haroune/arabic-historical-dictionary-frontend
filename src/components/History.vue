0b67755497f095f2e1c9d93deda6443953051f76
mohammedi <hm@big-mama.io>
2018-12-08 16:30:20 +0100

    add componenet History for word apparition in history

---
+++ src/components/History.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card
+  >
+    <v-card-text>
+      <v-timeline
+        align-top
+        dense
+      >
+        <v-timeline-item
+          color="pink"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs1>
+              <strong>5pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>New Icon</strong>
+              <div class="caption">Mobile App</div>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+
+        <v-timeline-item
+          color="teal lighten-3"
+          small
+        >
+          <v-layout wrap pt-3>
+            <v-flex xs1>
+              <strong>3-4pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Design Stand Up</strong>
+              <div class="caption mb-2">Hangouts</div>
+              <v-avatar>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+
+        <v-timeline-item
+          color="pink"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs1>
+              <strong>12pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Lunch break</strong>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+
+        <v-timeline-item
+          color="teal lighten-3"
+          small
+        >
+          <v-layout pt-3>
+            <v-flex xs1>
+              <strong>9-11am</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Finish Home Screen</strong>
+              <div class="caption">Web App</div>
+            </v-flex>
+          </v-layout>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'History'
+  }
+</script>
+
+<style scoped>
+
+</style>
