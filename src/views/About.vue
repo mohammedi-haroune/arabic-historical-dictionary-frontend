970ce88afee10491139372b4deba43edeb882a70
mohammedi <hm@big-mama.io>
2018-12-09 05:46:02 +0100

    view env vars in About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,8 @@
   <v-container row fluid>
     <h1>Environment variables</h1>
     <h2> {{ this.process.env }} </h2>
-    <h2> VEU_APP_TAHAR : {{ tahar }} </h2>
-    <h3 v-for="(env, i) in process.env" :key="i">
-      {{ env  }}
+    <h3 v-for="(k, v) in env" :key="k">
+      {{ k + ':'  + v }}
     </h3>
   </v-container>
 </template>
@@ -14,8 +13,7 @@ export default {
   name: 'About',
   data () {
     return {
-      process: process,
-      tahar: process.env
+      env: process.env
     }
   }
 }
