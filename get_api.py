5ed8877d0e8121d9115a62f3cc297ab0cd8c29f3
mohammedi <hm@big-mama.io>
2018-12-09 06:21:19 +0100

    write get_api.py script to get external ip and target port of the loadbancer of tal-backend service

---
+++ get_api.py
@@ -0,0 +1,10 @@
+import os
+from kubernetes import client, config
+
+config.load_incluster_config()
+v1=client.CoreV1Api()
+
+service = v1.read_namespaced_service('tal-backend', 'default')
+ip = service.status.load_balancer.ingress[0].ip
+port = service.spec.ports[0].port
+print(ip + ':' + port)
