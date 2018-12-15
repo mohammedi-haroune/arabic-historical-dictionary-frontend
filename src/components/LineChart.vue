39ec72d04224f7b8f6bdfca2517555f6012046c5
Wissben <wissam.benhaddad.pro@gmail.com>
2018-12-15 15:04:17 +0100

    FIXING : graphs

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,132 +1,26 @@
 <script>
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
+import { Line, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
 
-  return color;
-}
-
-function convertHex(hex) {
-  hex = hex.replace("#", "");
-  r = parseInt(hex.substring(0, 2), 16);
-  g = parseInt(hex.substring(2, 4), 16);
-  b = parseInt(hex.substring(4, 6), 16);
-
-  return [r, g, b];
-}
-
-function colorDistance(color1, color2) {
-  rgb1 = convertHex(color1);
-  rgb2 = convertHex(color2);
-
-  return Math.sqrt(
-    (rgb2[0] - rgb1[0]) ^
-      (2 + (rgb2[1] - rgb1[1])) ^
-      (2 + (rgb2[1] - rgb1[1])) ^
-      2
-  );
-}
-
-function allDist(col, cols, thresh) {
-  for (let o = 0; o < cols.length; o++) {
-    console.log(colorDistance(col, cols[o]));
-
-    if (colorDistance(col, cols[o]) < thresh) return false;
-  }
-  return true;
-}
-
-var dummy_data = [];
-var colors = [];
-for (let i = 0; i < 5; i++) {
-  var random = [];
-  random[0] = 10;
-  for (let j = 1; j < 12; j++) {
-    random.push(Math.random() * (100 - random[j - 1]) + 20);
-  }
-
-  var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  while (col in colors) {
-    col = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  }
-  colors.push(col);
-
-  dummy_data.push({
-    label: "Data " + i,
-    borderColor: col,
-    // borderColor: window.chartColors.blue,
-    // backgroundColor: window.chartColors.blue,
-    fill: false,
-    // pointBackgroundColor: "white",
-    // borderWidth: 1,
-    // pointBorderColor: "#249EBF",
-    // fill: false,
-    // Data to be represented on y-axis
-    data: random
-  });
-}
-
-// Importing Line class from the vue-chartjs wrapper
-import { Line } from "vue-chartjs";
-
-// Exporting this so it can be used in other components
 export default {
   extends: Line,
-  data() {
-    return {
-      datacollection: {
-        // Data to be represented on x-axis
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
-        ],
-        datasets: dummy_data // some array of values of lenght equals to the lenghts of LABLES
-      },
-      // Chart.js options that controls the appearance of the chart
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              },
-              gridLines: {
-                display: true
-              }
-            }
-          ],
-          xAxes: [
-            {
-              gridLines: {
-                display: true
-              }
-            }
-          ]
-        },
-        legend: {
-          display: true
-        },
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    };
-  },
+  mixins: [reactiveProp],
+  props: ["options"],
   mounted() {
-    // renderChart function renders the chart with the datacollection and options object.
-    this.renderChart(this.datacollection, this.options);
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, {
+      responvive: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: false
+            }
+          }
+        ]
+      }
+    });
   }
 };
 </script>
--- src/views/Graphs.vue
+++ src/views/Graphs.vue
@@ -1,19 +1,118 @@
 <template>
-<v-flex>
-  <LineChart></LineChart>
-  <LineChart></LineChart>
-</v-flex>
-  
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex v-for="d in datacollections" :key="d.datasets" xs4>
+        <template>
+          <line-chart :chart-data="d"></line-chart>
+        </template>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import LineChart from '@/components/LineChart'
+import LineChart from "@/components/LineChart";
 
-  
-  var components = {LineChart,LineChart}
-  export default {
-    name: 'Graphs',
-    components: components
+export default {
+  components: {
+    LineChart
+  },
+
+  data() {
+    return {
+      datacollections: [],
+      labels: []
+    };
+  },
+  mounted() {
+    // this.getDataFromQuery();
+    this.getDataFromQuery();
+    // console.log(this.datacollections);
+    // this.datacollection = this.fillData();
+  },
+
+  methods: {
+    fillData(labes, datasets) {
+      return {
+        // Data to be represented on x-axis
+        labels: this.labels,
+        datasets: this.getDummyData()
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+
+    async getDataFromQuery() {
+      // const docs = await $backend.$getStats(args);
+      //Must convert data here and place them into this.datacollections
+      /**
+       * The data must be converted to this 
+       * For each word in the result, the following object must be added :
+       * 
+       * [
+       *  {
+            label: "Meaning_1 of the word",
+            borderColor: "some color",
+            fill: false,
+            data: ["era_1:meaning_appartion" , "era_2:meaning_apparition" ... "era_n:meaning_apparation"]
+          },
+          ...
+          {
+            label: "Meaning_n of the word",
+            borderColor: "some color",
+            fill: false,
+            data: ["era_1:meaning_appartion" , "era_2:meaning_apparition" ... "era_n:meaning_apparation"]
+          },
+        ]
+       *
+       *In the end an array of these arrays must be constructed, it will be the datacollection of each 
+       *word array it represents a set of graphs, each graph is attached to a word.
+       *
+       *
+       * 
+       * 
+       *  
+       */
+      this.labels = ["Jahili", "Islamic", "Abbassi", "Modern"];
+      for (var i = 0; i < 5; i++) {
+        this.datacollections.push({
+          labels: this.labels,
+          datasets: this.getDummyData()
+        });
+      }
+    },
+
+    getDummyData() {
+      var dummy_data = [];
+      var colors = [];
+      for (let i = 0; i < 2; i++) {
+        var random = [];
+        random[0] = Math.random() * (100 - 0) + 20;
+        for (let j = 1; j < this.labels.length; j++) {
+          random.push(Math.random() * (100 - random[j - 1]) + 20);
+        }
+
+        var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+        dummy_data.push({
+          label: "Meaning " + i,
+          borderColor: col,
+          fill: false,
+          lineTension: 0.1,
+          borderWidth: 1.5,
+          data: random
+        });
+      }
+      return dummy_data;
+    }
   }
+};
 </script>
 
+<style>
+.small {
+  max-width: 600px;
+  margin: 150px auto;
+}
+</style>
