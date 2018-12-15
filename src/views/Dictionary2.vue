07211b1755a9e1596ba0c4296d1729b9222556b3
mohammedi <hm@big-mama.io>
2018-12-15 02:32:45 +0100

    feature[Dictionary2]: add loading progress circle when feteching data

--- src/views/Dictionary2.vue
+++ src/views/Dictionary2.vue
@@ -8,6 +8,7 @@
 <template>
   <v-container>
     <v-layout>
+      <!-- lazy here is required because otherwise selected_meaning and selected_term will be empty -->
       <v-dialog lazy v-model="dialog_add">
         <v-card>
           <v-toolbar>
@@ -18,7 +19,7 @@
               <v-icon color="red">close</v-icon>
             </v-btn>
           </v-toolbar>
-          <new-entry :page="page" :term_to_insert="selected_term"></new-entry>
+          <new-entry :page="page" :term_to_insert="selected_term" :meaning_to_insert="selected_meaning"></new-entry>
         </v-card>
       </v-dialog>
       <v-dialog lazy v-model="dialog_history">
@@ -44,6 +45,7 @@
     <v-layout>
     <v-flex text-xs-center>
         <v-pagination
+          v-if="num_pages > 0"
           @input="fetchEntries"
           total-visible="15"
           v-model="page"
@@ -51,7 +53,17 @@
         ></v-pagination>
       </v-flex>
     </v-layout>
-    <v-layout>
+    <v-layout  v-if="loading">
+      <v-flex pa-5 ma-5 xs10 text-xs-center>
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="primary"
+          indeterminate
+        ></v-progress-circular>
+      </v-flex>
+    </v-layout>
+    <v-layout v-else>
       <v-flex xs12>
         <ol>
           <li
@@ -66,7 +78,7 @@
                   :key="meaning.id"
                 >
                   {{ meaning.posTag ? meaning.posTag + ' : ' : '' }} {{ meaning.text }}
-                  <v-btn icon @click="add(meaning)">
+                  <v-btn icon @click="add(item.term, meaning)">
                     <v-icon>add</v-icon>
                   </v-btn>
                   <v-btn icon @click="history(meaning)">
@@ -101,7 +113,8 @@ export default {
       selected_term: '',
       selected_meaning: '',
       dialog_history: false,
-      clicked: false
+      clicked: false,
+      loading: false
     }
   },
   mounted () {
@@ -109,12 +122,14 @@ export default {
   },
   methods: {
     async fetchEntries () {
+      this.loading = true
       return $backend.$getEntrySet(this.page)
         .then(response => {
           const set = response.results
           this.items.length = 0
           this.items.push(...set)
           this.num_pages = Math.round(response.count / 12)
+          this.loading = false
         })
         .catch(err => console.warn(err))
     },
@@ -122,16 +137,18 @@ export default {
       const meaning = await $backend.$getMeaning(id)
       return meaning
     },
-    add (term) {
+    add (term, meaning) {
       console.log('term', term)
       this.selected_term = term
+      this.selected_meaning = meaning
       console.log('selected_term', this.selected_term)
+      console.log('selected_meaning', this.selected_meaning)
       this.dialog_add = true
     },
     history (meaning) {
-      console.log('term', meaning)
+      console.log('meaning', meaning)
       this.selected_meaning = meaning
-      console.log('selected_term', this.selected_term)
+      console.log('selected_meaning', this.selected_meaning)
       this.dialog_history = true
     }
   }
