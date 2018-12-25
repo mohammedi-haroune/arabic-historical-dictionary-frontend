a351a8d7a2e505df3744d70c1dae80a64d66da29
mohammedi <mohammedi.haroun@gmail.com>
2018-12-25 01:11:11 +0100

    feature: add prints

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-docker build -t gcr.io/arabic-historical-dictionary/tal-frontend .
-docker push gcr.io/arabic-historical-dictionary/tal-frontend
-kubectl delete -f tal-frontend.yaml
-kubectl create -f tal-frontend.yaml
+echo -e "\e[36mPULLING FRONTEND REPO\e[0m"
+git pull
+echo -e "\e[36mBUILDING FRONTEND IMAGE\e[0m"
+docker build -t gcr.io/arabic-historical-dictionary/tal-frontend-us .
+echo -e "\e[36mPUSHING FRONTEND IMAGE\e[0m"
+docker push gcr.io/arabic-historical-dictionary/tal-frontend-us
+echo -e "\e[36mDELETING DEPLOYMEMENT tal.yaml\e[0m"
+kubectl delete -f tal.yaml
+echo -e "\e[36mCREATING DEPLOYMENET tal.yaml\e[0m"
+kubectl create -f tal.yaml
