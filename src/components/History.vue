d04246fa17cebe10e63ed92813bd5867755a50e6
MOHAMMEDI Haroune <mohammedi.haroun@gmail.com>
2018-12-27 22:45:13 +0100

    feature[History]: add loading progress bar, remove useless commented code

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,151 +1,48 @@
+<i18n>
+  ar:
+    message:
+      error: "حدث خطأ عند إنشاء المصطلح"
+</i18n>
 <template>
-  <v-timeline>
-    <template v-for="(period, i) in sorted_periods">
-      <v-timeline-item
-        :key="i"
-        :color="colors[i]"
-        fill-dot
-        :right="i % 2 !== 0"
-        :left="i % 2 === 0"
-        :small="i % 2 !== 0"
-        v-if="appears_for(period.id).length > 0"
-      >
-        <v-hover>
-          <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 8 : 1}`">
-            <v-card-title :class="colors[i]">
-              <!-- <v-icon dark size="42">mdi-magnify</v-icon> -->
-              <v-icon dark size="42">mdi-calendar-text</v-icon>
-              <h2 class="display-1 white--text font-weight-light">{{ period.name }}</h2>
-            </v-card-title>
-            <v-container>
-              <v-layout>
-                <v-flex xs10>
-                  <ul>
-                    <li :key="j" v-for="(sentence, j) in appears_for(period.id)">{{ sentence }}</li>
-                  </ul>
-                </v-flex>
-                <v-flex xs2>
-                  <!-- <v-icon size="64">mdi-calendar-text</v-icon> -->
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card>
-        </v-hover>
-      </v-timeline-item>
-    </template>
-    <!--
-    <v-timeline-item
-      color="amber lighten-1"
-      fill-dot
-      left
-      small
-    >
-      <v-card>
-        <v-card-title class="amber lighten-1 justify-end">
-          <h2 class="display-1 mr-3 white&#45;&#45;text font-weight-light">Title 2</h2>
-          <v-icon
-            dark
-            size="42"
-          >mdi-home-outline</v-icon>
-        </v-card-title>
-        <v-container>
-          <v-layout>
-            <v-flex xs8>
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
-            </v-flex>
-            <v-flex xs4>
-              Lorem ipsum dolor sit amet, no nam oblique veritus.
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item
-      color="cyan lighten-1"
-      fill-dot
-      right
-    >
-      <v-card>
-        <v-card-title class="cyan lighten-1">
-          <v-icon
-            class="mr-3"
-            dark
-            size="42"
-          >
-            mdi-email-outline
-          </v-icon>
-          <h2 class="display-1 white&#45;&#45;text font-weight-light">Title 3</h2>
-        </v-card-title>
-        <v-container>
-          <v-layout>
-            <v-flex
-              v-for="n in 3"
-              :key="n"
-              xs4
-            >
-              Lorem ipsum dolor sit amet, no nam oblique veritus no nam oblique.
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item
-      color="red lighten-1"
-      fill-dot
-      left
-      small
-    >
-      <v-card>
-        <v-card-title class="red lighten-1 justify-end">
-          <h2 class="display-1 mr-3 white&#45;&#45;text font-weight-light">Title 4</h2>
-          <v-icon
-            dark
-            size="42"
-          >
-            mdi-account-multiple-outline
-          </v-icon>
-        </v-card-title>
-        <v-container>
-          <v-layout>
-            <v-flex xs2>
-              <v-icon size="64">mdi-server-network</v-icon>
-            </v-flex>
-            <v-flex xs10>
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus.
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item
-      color="green lighten-1"
-      fill-dot
-      right
-    >
-      <v-card>
-        <v-card-title class="green lighten-1">
-          <v-icon
-            class="mr-3"
-            dark
-            size="42"
-          >
-            mdi-phone-in-talk
-          </v-icon>
-          <h2 class="display-1 white&#45;&#45;text font-weight-light">Title 5</h2>
-        </v-card-title>
-        <v-container>
-          <v-layout>
-            <v-flex>
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-timeline-item>-->
-  </v-timeline>
+  <div>
+    <v-alert v-if="error" :value="error" type="error" outline>{{ $t('message.error') }}</v-alert>
+    <v-progress-circular class="text-xs-center" v-if="loading" :size="70" :width="7" color="primary" indeterminate></v-progress-circular>
+    <v-timeline v-else>
+      <template v-for="(period, i) in sorted_periods">
+        <v-timeline-item
+          :key="i"
+          :color="colors[i]"
+          fill-dot
+          :right="i % 2 !== 0"
+          :left="i % 2 === 0"
+          :small="i % 2 !== 0"
+          v-if="appears_for(period.id).length > 0"
+        >
+          <v-hover>
+            <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 8 : 1}`">
+              <v-card-title :class="colors[i]">
+                <!-- <v-icon dark size="42">mdi-magnify</v-icon> -->
+                <v-icon dark size="42">mdi-calendar-text</v-icon>
+                <h2 class="display-1 white--text font-weight-light">{{ period.name }}</h2>
+              </v-card-title>
+              <v-container>
+                <v-layout>
+                  <v-flex xs10>
+                    <ul>
+                      <li :key="j" v-for="(sentence, j) in appears_for(period.id)">{{ sentence }}</li>
+                    </ul>
+                  </v-flex>
+                  <v-flex xs2>
+                    <!-- <v-icon size="64">mdi-calendar-text</v-icon> -->
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-hover>
+        </v-timeline-item>
+      </template>
+    </v-timeline>
+  </div>
 </template>
 
 <script>
@@ -163,6 +60,8 @@ export default {
   data() {
     return {
       appears_set: [],
+      loading: false,
+      error: false,
       colors: [
         "cyan lighten-1",
         "purple lighten-2",
@@ -184,11 +83,14 @@ export default {
     ...mapState(["periods"])
   },
   methods: {
-    async fetchAppears() {
+    async fetchAppears () {
+      this.loading = true
       console.log("fetch for ", this.meaning_id);
       $bakcend.$fetchAppears(this.meaning_id).then(results => {
         console.log(results);
         this.appears_set = results.appears_set;
+        this.loading = false
+        this.error = false
         /* results.appears_set.forEach(appear => {
             if (typeof this.appears_set[appear.period_id] === 'undefined') {
               this.appears_set[appear.period_id] = [appear.sentence]
@@ -196,7 +98,10 @@ export default {
               this.appears_set[appear.period_id].push(appear.sentence)
             }
           }) */
-      });
+      }).catch(function (err) {
+        console.log('err from history', err)
+        // this.error = true
+      })
     },
     appears_for(id) {
       return this.appears_set
