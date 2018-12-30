846835e7673018a8cb122022ebd666dbcce7d523
MOHAMMEDI Haroune <mohammedi.haroun@gmail.com>
2018-12-30 23:39:53 +0100

    feature[Graphs]: handle loading, error, no_data cases

--- src/components/Graphs.vue
+++ src/components/Graphs.vue
@@ -1,6 +1,18 @@
+<i18n>
+  ar:
+    message:
+      error: "حدث خطأ"
+      no_data: "غير متوفر"
+</i18n>
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
+    <v-flex text-xs-center>
+      <v-alert v-if="error" :value="error" type="error" outline>{{ $t('message.error') }}</v-alert>
+      <v-alert :value="!loading && !error && datacollections.length === 0" type="warning" outline>{{ $t('message.no_data') }}</v-alert>
+      <v-progress-circular class="text-xs-center" v-if="loading" :size="70" :width="7" color="primary" indeterminate></v-progress-circular>
+    </v-flex>
+     <template v-if="!error && !loading && datacollections.length > 0">
       <v-flex v-for="(d, i) in datacollections" :key="i" xs4>
         <v-hover>
           <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 10 : 0}`">
@@ -12,6 +24,7 @@
           </v-card>
         </v-hover>
       </v-flex>
+     </template>
     </v-layout>
   </v-container>
 </template>
@@ -19,6 +32,7 @@
 <script>
 import LineChart from "../components/LineChart";
 import $backend from "../backend";
+import _ from 'lodash'
 
 export default {
   name: 'Graphs',
@@ -36,7 +50,9 @@ export default {
       show: false,
       dict: {},
       meaning: "None",
-      show: false
+      show: false,
+      loading: false,
+      error: false
     };
   },
   mounted() {
@@ -44,72 +60,73 @@ export default {
   },
 
   methods: {
-    async getStatistics(id) {
-      return $backend
-        .$getStatistics(id)
-        .then(response => {
-          this.stats = response;
-        })
-        .catch(err => console.warn(err));
-      // console.table(meaning);
-    },
     async getDataFromQuery() {
-      this.stats = await $backend.$getStatisticsById(this.id);
-      var target = { ...this.stats };
-      var keys = Object.keys(this.stats);
-      var del = keys.shift();
-      for (key in keys) {
-        var keysStats = Object.keys(this.stats[keys[key]]["stats"]);
-        for (var keystat in keysStats) {
-          var keyCategs = Object.keys(
-            this.stats[keys[key]]["stats"][keysStats[keystat]]
-          );
-          for (var keyCateg in keyCategs) {
-            target[del]["stats"][keysStats[keystat]][
-              keyCategs[keyCateg]
-            ] += this.stats[keys[key]]["stats"][keysStats[keystat]][
-              keyCategs[keyCateg]
-            ];
-          }
-        }
-      }
-      this.stats = target;
-      delete target[0];
-      const k = Object.keys(this.stats);
-      this.meaning = this.id;
-      const eras = this.stats[k[0]]["stats"];
-      this.labels = Object.keys(eras);
-      const categories = Object.keys(eras[this.labels[0]]);
+      this.loading = true
+      try {
+        this.stats = await $backend.$getStatisticsById(this.id);
+        if (!_.isEmpty(this.stats)) {
 
-      // const categsCount ;
-      for (var key in categories) {
-        this.dict[categories[key]] = [];
-        for (var key2 in eras) {
-          // console.log(categories[key]); // Category
-          // console.log(key2); // Era
-          // console.log(eras[key2][categories[key]]); Category in that era
-          this.dict[categories[key]].push(eras[key2][categories[key]]);
-        }
-        // console.log(this.dict);
-      }
-
-      for (var cat in this.dict) {
-        var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-        this.datacollections.push({
-          labels: this.labels,
-          datasets: [
-            {
-              label: cat,
-              borderColor: col,
-              fill: false,
-              lineTension: 0.1,
-              borderWidth: 5,
-              data: this.dict[cat]
+          var target = { ...this.stats };
+          var keys = Object.keys(this.stats);
+          var del = keys.shift();
+          for (key in keys) {
+            var keysStats = Object.keys(this.stats[keys[key]]["stats"]);
+            for (var keystat in keysStats) {
+              var keyCategs = Object.keys(
+                this.stats[keys[key]]["stats"][keysStats[keystat]]
+              );
+              for (var keyCateg in keyCategs) {
+                target[del]["stats"][keysStats[keystat]][
+                  keyCategs[keyCateg]
+                ] += this.stats[keys[key]]["stats"][keysStats[keystat]][
+                  keyCategs[keyCateg]
+                ];
+              }
             }
-          ]
-        });
+          }
+          this.stats = target;
+          delete target[0];
+          const k = Object.keys(this.stats);
+          this.meaning = this.id;
+          const eras = this.stats[k[0]]["stats"];
+          this.labels = Object.keys(eras);
+          const categories = Object.keys(eras[this.labels[0]]);
+
+          // const categsCount ;
+          for (var key in categories) {
+            this.dict[categories[key]] = [];
+            for (var key2 in eras) {
+              // console.log(categories[key]); // Category
+              // console.log(key2); // Era
+              // console.log(eras[key2][categories[key]]); Category in that era
+              this.dict[categories[key]].push(eras[key2][categories[key]]);
+            }
+            // console.log(this.dict);
+          }
+
+          for (var cat in this.dict) {
+            var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+            this.datacollections.push({
+              labels: this.labels,
+              datasets: [
+                {
+                  label: cat,
+                  borderColor: col,
+                  fill: false,
+                  lineTension: 0.1,
+                  borderWidth: 5,
+                  data: this.dict[cat]
+                }
+              ]
+            });
+          }
+        } 
+      } catch (err) {
+        console.error(err)
+        this.error = true
       }
+      this.loading = false
     }
 
     // getDummyData() {
