e59db28b3dcaaa2258156df5080d6a47f26c4b53
Wissben <wissam.benhaddad.pro@gmail.com>
2019-01-05 02:02:57 +0100

    Changing graphs behaviour

--- src/components/Graphs.vue
+++ src/components/Graphs.vue
@@ -7,24 +7,36 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-    <v-flex text-xs-center>
-      <v-alert v-if="error" :value="error" type="error" outline>{{ $t('message.error') }}</v-alert>
-      <v-alert :value="!loading && !error && datacollections.length === 0" type="warning" outline>{{ $t('message.no_data') }}</v-alert>
-      <v-progress-circular class="text-xs-center" v-if="loading" :size="70" :width="7" color="primary" indeterminate></v-progress-circular>
-    </v-flex>
-     <template v-if="!error && !loading && datacollections.length > 0">
-      <v-flex v-for="(d, i) in datacollections" :key="i" xs4>
-        <v-hover>
-          <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 10 : 0}`">
-            <template>
-              <v-spacer></v-spacer>
-              <line-chart :chart-data="d"></line-chart>
-              <v-spacer></v-spacer>
-            </template>
-          </v-card>
-        </v-hover>
+      <v-flex text-xs-center>
+        <v-alert v-if="error" :value="error" type="error" outline>{{ $t('message.error') }}</v-alert>
+        <v-alert
+          :value="!loading && !error && datacollections.length === 0"
+          type="warning"
+          outline
+        >{{ $t('message.no_data') }}</v-alert>
+        <v-progress-circular
+          class="text-xs-center"
+          v-if="loading"
+          :size="70"
+          :width="7"
+          color="primary"
+          indeterminate
+        ></v-progress-circular>
       </v-flex>
-     </template>
+      <template v-if="!error && !loading && datacollections.length > 0">
+        <v-flex v-for="(d, i) in datacollections" :key="i" xs4>
+          <v-hover>
+            <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 10 : 0}`">
+              <v-card-title class="primary white--text headline">{{ d.categ }}</v-card-title>
+              <template>
+                <v-spacer></v-spacer>
+                <line-chart :chart-data="d"></line-chart>
+                <v-spacer></v-spacer>
+              </template>
+            </v-card>
+          </v-hover>
+        </v-flex>
+      </template>
     </v-layout>
   </v-container>
 </template>
@@ -32,10 +44,10 @@
 <script>
 import LineChart from "../components/LineChart";
 import $backend from "../backend";
-import _ from 'lodash'
+import _ from "lodash";
 
 export default {
-  name: 'Graphs',
+  name: "Graphs",
   components: {
     LineChart
   },
@@ -49,7 +61,6 @@ export default {
       labels: [],
       stats: {},
       show: false,
-      dict: {},
       meaning: "None",
       show: false,
       loading: false,
@@ -58,76 +69,111 @@ export default {
   },
   mounted() {
     this.getDataFromQuery();
+    console.log("mounted with ", this.datacollections);
   },
 
   methods: {
     async getDataFromQuery() {
-      this.loading = true
+      this.loading = true;
       try {
         this.stats = await $backend.$getStatisticsById(this.word_id);
         if (!_.isEmpty(this.stats)) {
-
-          var target = { ...this.stats };
-          var keys = Object.keys(this.stats);
-          var del = keys.shift();
-          for (key in keys) {
-            var keysStats = Object.keys(this.stats[keys[key]]["stats"]);
-            for (var keystat in keysStats) {
-              var keyCategs = Object.keys(
-                this.stats[keys[key]]["stats"][keysStats[keystat]]
-              );
-              for (var keyCateg in keyCategs) {
-                target[del]["stats"][keysStats[keystat]][
-                  keyCategs[keyCateg]
-                ] += this.stats[keys[key]]["stats"][keysStats[keystat]][
-                  keyCategs[keyCateg]
-                ];
-              }
-            }
-          }
-          this.stats = target;
-          delete target[0];
           const k = Object.keys(this.stats);
           this.meaning = this.id;
-          const eras = this.stats[k[0]]["stats"];
-          this.labels = Object.keys(eras);
-          const categories = Object.keys(eras[this.labels[0]]);
+          const dict = {};
 
-          // const categsCount ;
-          for (var key in categories) {
-            this.dict[categories[key]] = [];
-            for (var key2 in eras) {
-              // console.log(categories[key]); // Category
-              // console.log(key2); // Era
-              // console.log(eras[key2][categories[key]]); Category in that era
-              this.dict[categories[key]].push(eras[key2][categories[key]]);
+          if (!this.meaning_id) {
+            const inner_dict = [...Array(k.length - 1)];
+            for (let i = 0; i < k.length - 1; i++) {
+              inner_dict[i] = {};
+              const eras = this.stats[k[i]]["stats"];
+              this.labels = this.stats["ordered_eras"];
+              const categories = Object.keys(eras[this.labels[0]]);
+              // const categsCount ;
+
+              for (var key in categories) {
+                inner_dict[i][categories[key]] = [];
+                for (var key2 in eras) {
+                  // console.log(categories[key]); // Category
+                  // console.log(key2); // Era
+                  // console.log(eras[key2][categories[key]]); Category in that era
+                  inner_dict[i][categories[key]].push(
+                    eras[key2][categories[key]]
+                  );
+                }
+              }
+              // console.log("this is inner at %d ", i, inner_dict);
             }
-            // console.log(this.dict);
-          }
 
-          for (var cat in this.dict) {
-            var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
+            for (var cat in inner_dict[0]) {
+              var datas = [];
+              for (let i = 0; i < k.length - 1; i++) {
+                // console.log("this is inner ", inner_dict[i][cat]);
 
-            this.datacollections.push({
-              labels: this.labels,
-              datasets: [
-                {
-                  label: cat,
+                var col =
+                  "#" + Math.floor(Math.random() * 16777215).toString(16);
+                datas.push({
+                  label: this.stats[k[i]]["meaning"],
                   borderColor: col,
                   fill: false,
                   lineTension: 0.1,
                   borderWidth: 5,
-                  data: this.dict[cat]
-                }
-              ]
-            });
+                  data: inner_dict[i][cat]
+                });
+              }
+              this.datacollections.push({
+                categ: cat,
+                labels: this.labels,
+                datasets: datas
+              });
+            }
+          } else {
+            var index = 0;
+            for (let i = 0; i < k.length; i++) {
+              if (k[i] == this.meaning_id) {
+                index = i;
+              }
+            }
+            const eras = this.stats[k[index]]["stats"];
+            this.labels = this.stats["ordered_eras"];
+            const categories = Object.keys(eras[this.labels[0]]);
+
+            // const categsCount ;
+            for (var key in categories) {
+              dict[categories[key]] = [];
+              for (var key2 in eras) {
+                // console.log(categories[key]); // Category
+                // console.log(key2); // Era
+                // console.log(eras[key2][categories[key]]); Category in that era
+                dict[categories[key]].push(eras[key2][categories[key]]);
+              }
+              // console.log(this.dict);
+            }
+            for (var cat in dict) {
+              var col = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+              this.datacollections.push({
+                categ: cat,
+                labels: this.labels,
+                datasets: [
+                  {
+                    label: this.stats[this.meaning_id]["meaning"],
+                    borderColor: col,
+                    fill: false,
+                    lineTension: 0.1,
+                    borderWidth: 5,
+                    data: dict[cat]
+                  }
+                ]
+              });
+            }
           }
-        } 
+        }
       } catch (err) {
-        console.error(err)
-        this.error = true
+        console.error(err);
+        this.error = true;
       }
-      this.loading = false
+      this.loading = false;
     }
 
     // getDummyData() {
